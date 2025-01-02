7eef8c057ad6adef4121b3ee4618b656b64acdd4
Andrey <dr.af2015@yandex.ru>
2025-01-02 17:33:28 +0300

    выполнено

--- module_14/task_01/main.py
+++ module_14/task_01/main.py
@@ -2,7 +2,7 @@ import sqlite3
 
 connection = sqlite3.connect('not_telegram.db')
 cursor = connection.cursor()
-# cursor.execute('CREATE INDEX IF NOT EXISTS idx_email ON Users (email)')
+cursor.execute('CREATE INDEX IF NOT EXISTS idx_email ON Users (email)')
 
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS Users(
